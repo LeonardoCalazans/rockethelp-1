a9848792c61b8d4a1c1ae025d07469a250b963bc
migueelzz <miguellemes005@gmail.com>
2022-07-20 15:13:26 -0300

    :fire: class 3 finished
    Integration with firebase

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useNavigation } from '@react-navigation/native';
+import { Alert } from 'react-native';
 import { HStack, IconButton, VStack, useTheme, Text, Heading, FlatList, Center } from 'native-base';
+
 import { SignOut, ChatTeardropText } from 'phosphor-react-native';
+
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
 
 import { Filter } from '../components/Filter';
 import { Button } from '../components/Button';
 import { Order, OrderProps } from '../components/Order';
+import { Loading } from '../components/Loading';
+
+import { dateFormat } from '../utils/firestoreDateFormat';
 
 import Logo from '../assets/logo_secondary.svg';
 
 export function Home() {
+  const [isLoading, setIsLoading] = useState(true);
   const navigation = useNavigation();
   const { colors } = useTheme();
 
   const [statusSelected, setStatusSelected] = useState<'open' |'closed'>('open');
 
-  const [orders, setOrders] = useState<OrderProps[]>([
-    {
-      id: '123',
-      patrimony: '123456',
-      when: '18/07/2022 às 14:00',
-      status: 'open',
-    },
-    {
-      id: '456',
-      patrimony: '78910',
-      when: '17/07/2022 às 21:00',
-      status: 'open',
-    },
-  ]);
+  const [orders, setOrders] = useState<OrderProps[]>([]);
 
   function handleNewOrder() {
     navigation.navigate('new');
@@ -37,6 +33,40 @@ export function Home() {
   function handleOpenDetails(orderId: string) {
     navigation.navigate('details', { orderId });
   }
+
+  function handleLogout() {
+    auth()
+    .signOut()
+    .catch((err) => {
+      console.log(err);
+      return Alert.alert('Sair', 'Não foi possível sair.')
+    })
+  }
+
+  useEffect(() => {
+    setIsLoading(true);
+    const subscriber = firestore()
+    .collection('orders')
+    .where('status', '==', statusSelected)
+    .onSnapshot(snapshot => {
+      const data = snapshot.docs.map(doc => {
+        const { patrimony, description, status, created_at } = doc.data();
+
+        return {
+          id: doc.id,
+          patrimony,
+          description,
+          status,
+          when: dateFormat(created_at)
+        }
+      });
+
+      setOrders(data);
+      setIsLoading(false);
+    });
+
+    return subscriber;
+  }, [statusSelected]);
 
   return (
     <VStack flex={1} pb={6} bg="gray.700">
@@ -52,6 +82,7 @@ export function Home() {
       <Logo />
       <IconButton 
         icon={<SignOut size={26} color={colors.gray[300]}  />}
+        onPress={handleLogout}
       />
       </HStack>
 
@@ -82,7 +113,9 @@ export function Home() {
           />
         </HStack>
 
-        <FlatList 
+        {
+          isLoading ? <Loading /> :
+          <FlatList 
           data={orders}
           keyExtractor={item => item.id}
           renderItem={({ item }) => <Order data={item} onPress={() => handleOpenDetails(item.id)} />}
@@ -98,6 +131,7 @@ export function Home() {
             </Center>
           )}
         />
+        }
 
         <Button title="Nova solicitação" onPress={handleNewOrder}/>
 
--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -1,15 +1,55 @@
+import { useState, useEffect } from 'react';
+
 import { VStack } from 'native-base';
+import { Alert } from 'react-native';
+
+import { useNavigation } from'@react-navigation/native';
+
+import firestore from '@react-native-firebase/firestore';
+
 import { Header } from '../components/Header';
 import { Input } from '../components/Input';
 import { Button } from '../components/Button';
 
 export function Register() {
+  const navigation = useNavigation();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [patrimony, setPatrimony] = useState('');
+  const [description, setDescription] = useState('');
+
+  function handleNewOrderRegister() {
+    if(!patrimony || !description) {
+     return Alert.alert('Registrar', 'Preencha todos os campos.')
+    }
+
+    setIsLoading(true);
+    firestore()
+    .collection('orders')
+    .add({
+      patrimony,
+      description,
+      status: 'open',
+      created_at: firestore.FieldValue.serverTimestamp()
+    })
+    .then(() => {
+      Alert.alert('Solicitação', 'Solicitação registrada com sucesso.');
+      navigation.goBack();
+    })
+    .catch(err => {
+      console.log(err);
+      setIsLoading(false)
+      return Alert.alert('Solicitação', 'Não foi possível registrar o pedido.')
+    });
+  }
+
   return (
     <VStack flex={1} p={6} bg="gray.600">
-      <Header title="Nova solicitação" />
+      <Header title="Solicitação" />
       <Input 
         placeholder='Número do patrimônio'
         mt={4}
+        onChangeText={setPatrimony}
       />
       <Input 
         placeholder='Descrição do problema'
@@ -17,10 +57,13 @@ export function Register() {
         mt={5}
         multiline
         textAlignVertical='top'
+        onChangeText={setDescription}
       />
       <Button 
         title='Cadastrar'
         mt={5}
+        isLoading={isLoading}
+        onPress={handleNewOrderRegister} 
       /> 
     </VStack>
   );
